f011ae2a4c2d070f0879cb609f0975f6aa46d7da
bleach1425 <64241192+bleach1425@users.noreply.github.com>
2022-01-25 16:40:33 +0800

    Api_Test_offical_v1

---
+++ api_module.py
@@ -0,0 +1,113 @@
+import requests
+import json
+from api_package.decorater import runtime_check
+
+
+class Api:
+    def __init__(self, *args):
+        """""""""
+        args[0] = url
+        args[1] = GET or POST  //required//
+        args[2] = [send value(data)]
+        args[3] = [send value(json)]
+        args[4] = [send params]
+        args[5] = file  
+        args[6] = runtime
+        """""""""
+
+        print("args: ", args)
+
+        self.url = args[0]
+        self.method = args[1]
+        self.header = args[2]
+
+        if args[3]:
+            self.data = args[3]
+        else:
+            self.data = ''
+
+        if args[4]:
+            self.json = args[4]
+        else:
+            self.json = ''
+        
+        if args[5]:
+            self.param = args[5]
+        else:
+            self.param = ''
+
+        if args[6]:
+            self.file = {"file": open(args[6], 'rb')}
+        else:
+            self.file = ''
+
+        if args[7]:
+            self.runtime = args[7]
+        else:
+            self.runtime = 1
+
+    def api_test_handinput(self):
+        if self.method == "GET":
+            for run in range(int(self.runtime)):
+                r = requests.get(self.url, data= self.data, files=self.file, headers= self.header, json=self.json, params=self.param)
+                print(r.status_code, r.text)
+
+        elif self.method == "POST":
+            for run in range(self.runtime):
+                r = requests.post(self.url, data= self.data, files=self.file, headers= self.header, json=self.json, params=self.param)
+                print(r.status_code, r.text)
+
+    
+    @classmethod
+    def api_test_json(param):
+        pass
+
+
+
+@runtime_check(program='Api測試', coder='John')
+def main(*params):
+    if params[0] == 'handinput':
+        work = Api(params[1], params[2], params[3], params[4], params[5], params[6], params[7], params[8])
+        print('*'*8)
+        print("測試結果:")
+        work.api_test_handinput()
+        print('*'*8)
+        print('')
+
+    elif params[0] == 'json':
+        with open('api_test.json', newline='') as f:
+            '''
+            # Get Params
+            1. url
+            2. GET or POST  //required//
+            3. [send value(data)]
+            4. [send value(json)]
+            5. = [send params]
+            6. = file  
+            7. = runtime
+            '''
+            data = json.load(f)
+            # -------------------------- #
+            Url = data.get('Url')
+            Method = data.get('Method')
+            Header = data.get('Header')
+            Data = data.get('Data')
+            Json = data.get('Json')
+            Param = data.get("Param")
+            File = data.get("File")
+            Runtime = data.get('Runtime')
+            # -------------------------- #
+            if Method == 'GET':
+                for n in range(int(Runtime)):
+                    r = requests.post(Url, data= Data, files=File, headers= Header, json=Json, params=Param)
+                    print(r.status_code, r.text)
+
+            elif Method == "POST":
+                for n in range(int(Runtime)):
+                    r = requests.post(Url, data= Data, files=File, headers= Header, json=Json, params=Param)
+                    print(r.status_code, r.text)
+            
+        # Api.api_test_json()
+
+if __name__ == '__main__': 
+    main()
---
+++ api_package/decorater.py
@@ -0,0 +1,86 @@
+from functools import wraps
+import time
+
+def runtime_check(program, coder):
+    # 第二層回傳帶入func
+    def wrapper(func):
+        @wraps(func)
+        def inner_wrapper(*args, **kwargs):
+            """""""""
+            args[0] = url
+            args[1] = GET or POST  //required//
+            args[2] = [send value(data)]
+            args[3] = [send value(json)]
+            args[4] = [send params]
+            args[5] = file
+            args[6] = file_path
+            args[7] = runtime
+            """""""""
+            # Input Param First
+            print(f"Coder: {coder}")
+            print('')
+            print('*' * 8)
+            while True:
+                work_type = input("請輸入測試類型(`json` or `handinput`): ")
+                if work_type:
+                    if work_type != 'handinput' and work_type != 'json':
+                        print('請輸入 `handinput` or `json` 兩種測試類型二選一')
+                    else:
+                        break
+                else:
+                    print('您輸入的值為空請在試一次')
+
+            if work_type == 'handinput':
+                url = input("測試網址: ")
+                while True:
+                    method = input("接收方式(GET or POST): ")
+                    if method:
+                        if method != 'GET' and method != 'POST':
+                            print('請輸入 `GET` or `POST` 兩種測試類型二選一')
+                        else:
+                            break
+                    else:
+                        print('您輸入的值為空請在試一次')
+                
+                header = input("header參數: ")
+                data = input("data參數: ")
+                json = input("json參數: ")
+                params = input("params參數: ")
+
+                if data: 
+                    try:
+                        data = eval(data)
+                        json = eval(json)
+                        params = eval(params)
+                    except:
+                        raise ValueError("參數格式錯誤(Ex: {'Test': 'Item'})")
+                else:
+                    data = {"data": "default"}
+                    json = {"json": "default"}
+                    params = ''
+                
+                files = input("files參數: ")
+                runtime = input("請輸入要執行次數: ")
+                print('*' * 8)
+                print('')
+                # Start Program
+                start = time.time()
+                func(work_type, url, method, header, data, json, params, files, runtime)
+                end = time.time()
+                print(f"{program}: 用時 {round(end - start, 2)} 秒")
+                return "OK"
+
+            elif work_type == 'json':
+                print('*' * 8)
+                print('')
+                # Start Program
+                start = time.time()
+                func(work_type)
+                end = time.time()
+                print('')
+                print('*'*8)
+                print(f"{program}: 用時 {round(end - start, 2)} 秒")
+                return "OK"
+
+        return inner_wrapper
+    return wrapper
